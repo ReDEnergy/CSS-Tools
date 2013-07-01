6593404cf81be5edbd036be3a667bc206977518e
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2013-07-01 13:27:01 +0300

    layer management improved

--- tools/box-shadow/js/box-shadow.js
+++ tools/box-shadow/js/box-shadow.js
@@ -31,11 +31,32 @@ var BoxShadow = (function BoxShadow() {
 	 */
 	function Shadow() {
 		this.inset  = false;
-		this.posX   = 10;
-		this.posY   = 10;
+		this.posX   = 5;
+		this.posY   = -5;
 		this.blur   = 5;
 		this.spread = 0;
 		this.color  = [0, 0, 0, 1];
+		this.undo	= [];
+	}
+
+	Shadow.prototype.setGlow = function setGlow() {
+		this.saveState();
+		this.spread	= 10;
+		this.blur	= 10;
+		this.color	= [37, 123, 194, 1];
+	}
+
+	Shadow.prototype.saveState = function saveState() {
+		this.undo.push([this.posX, this.posY, this.blur, this.spread, this.color]);
+	}
+
+	Shadow.prototype.undoState = function undoState() {
+		var prev = this.undo.pop([this.posX, this.posY, this.blur, this.spread, this.color]);
+		this.posX	= prev[0];
+		this.posY	= prev[1];
+		this.blur	= prev[2];
+		this.spread	= prev[3];
+		this.color	= prev[4];
 	}
 
 	Shadow.prototype.computeCSS = function computeCSS() {
@@ -73,30 +94,49 @@ var BoxShadow = (function BoxShadow() {
 		}
 
 		var dragStart = function dragStart(e) {
+			if (e.button !== 0)
+				return;
+
 			active = true;
 			lastX = e.clientX;
 			lastY = e.clientY;
 			document.addEventListener('mousemove', mouseDrag, false);
-		};
+		}
 
 		var dragEnd = function dragEnd(e) {
+			if (e.button !== 0)
+				return;
+
 			if (active === true) {
 				active = false;
 				document.removeEventListener('mousemove', mouseDrag, false);
 			}
-		};
+		}
 
 		var mouseDrag = function mouseDrag(e) {
 			Tool.updateActivePos(e.clientX - lastX, e.clientY - lastY);
 			lastX = e.clientX;
 			lastY = e.clientY;
-		};
+		}
 
 		return {
 			init : init
 		}
 
 	})();
+
+	var Controls = (function Controls() {
+
+		var init = function init() {
+			preview.addEventListener("mwhell")
+		}
+
+		return {
+			init : init
+		}
+
+	})();
+
 
 
 
@@ -120,11 +160,28 @@ var BoxShadow = (function BoxShadow() {
 		}
 
 		var addShadow = function addShadow() {
-			active = new Shadow();
-			shodowID = shadows.length;
-			shadows.push(active);
-			renderShadow();
+
+			var length = shadows.push(new Shadow());
+			setActiveShadow(length - 1);
 			update();
+		}
+
+		var setActiveShadow = function setActiveShadow(id) {
+			if (shodowID === id)
+				return;
+
+			active = shadows[id];
+			addGlowEffect(id);
+			shodowID = id;
+
+			console.log("Shadows", shadows);
+			console.log("Render", render);
+		}
+
+		var setActiveObject =  function setActiveObject(item) {
+			shodowID = null;
+			if (item === 0)
+				active = subject;
 		}
 
 		var updateActivePos = function updateActivePos(deltaX, deltaY) {
@@ -134,18 +191,16 @@ var BoxShadow = (function BoxShadow() {
 				updateShadowPos(deltaX, deltaY);
 		}
 
+		var renderShadow = function renderShadow(id) {
+			render[id] = shadows[id].computeCSS();
+		}
+
 		var updateShadowPos = function updateShadowPos(deltaX, deltaY) {
 			active.posX += deltaX;
 			active.posY += deltaY;
-			render[shodowID] = active.computeCSS();
+			renderShadow(shodowID);
 			update();
 		}
-
-		var renderShadow = function renderShadow() {
-			render[shodowID] = active.computeCSS();
-		}
-
-
 		var updateObjPos = function updateObjPos(deltaX, deltaY) {
 			SubjectObj.posX += deltaX;
 			SubjectObj.posY += deltaY;
@@ -154,25 +209,47 @@ var BoxShadow = (function BoxShadow() {
 		}
 
 		var update = function update() {
-			subject.style.boxShadow = render.join(", ");
-			output.textContent = render.join("\n");
+			var outputCSS = [];
+			for (var i in render)
+				if (render[i])
+					outputCSS.push(render[i]);
+			output.textContent = outputCSS.join(", \n");
+
+			subject.style.boxShadow = outputCSS.join(", ");
+		}
+
+		var deleteShadow = function deleteShadow(id) {
+			delete shadows[id];
+			delete render[id];
+			if (shodowID === id)
+				active = subject;
+			update();
+		}
+
+		// Implement in shadow object the glow effect
+		// should return the previous shadow state
+		var addGlowEffect = function addGlowEffect(id) {
+			shadows[id].setGlow();
+			renderShadow(id);
+			subject.style.transition = "box-shadow 0.2s";
+			update();
+			setTimeout(function() {
+				shadows[id].undoState();
+				renderShadow(id);
+				update();
+				setTimeout(function() {
+					subject.style.removeProperty("transition");
+				}, 200);
+			}, 200);
 		}
 
 		return {
 			init : init,
 			addShadow : addShadow,
 			updateActivePos : updateActivePos,
-			updateShadowPos : updateShadowPos,
-			renderShadow : renderShadow,
-			updateObjPos : updateObjPos,
-			update : update,
-			getActiveNode : function (id) {
-				return active;
-			},
-			setActiveShadow : function setActiveShadow(id) {
-				active = shadows[id];
-				shodowID = id;
-			},
+			setActiveShadow : setActiveShadow,
+			setActiveObject : setActiveObject,
+			deleteShadow : deleteShadow
 		}
 
 	})();
@@ -182,77 +259,100 @@ var BoxShadow = (function BoxShadow() {
 	 * Layer Manager
 	 */
 	var LayerManager = (function LayerManager() {
-		var layer_ID = null;
+		var layer_ID = 0;
+		var activeLayer = null;
 		var nr = 0;
 		var order = [];
 		var stack;
 		var add_btn;
-
-		var init = function init() {
-			stack = getElemById("shadow_stack");
-			add_btn = getElemById("new_layer");
-			add_btn.addEventListener("click", addLayer);
-			stack.addEventListener("click", clickEvent);
+		var Subject = {
+			before : null,
+			after : null,
+			elem : null
 		}
 
-		var clickEvent = function clickEvent(e) {
+		function init() {
+			Subject.elem = getElemById("subject_layer");
+			Subject.before = getElemById("subject_before");
+			Subject.after = getElemById("subject_after");
+
+			stack = getElemById("shadow_stack");
+
+			add_btn = getElemById("new_layer");
+
+			add_btn.addEventListener("click", addLayer);
+			stack.addEventListener("click", clickShadowStack);
+			Subject.elem.addEventListener("click", activateSubject);
+
+			switchActiveLayerTo(Subject.elem);
+		}
+
+		var activateSubject = function activateSubject() {
+			switchActiveLayerTo(Subject.elem);
+			Tool.setActiveObject(0);
+		}
+
+		var clickShadowStack = function clickShadowStack(e) {
+			console.log(e.target);
 			if (e.target.className === "layer") {
-				getActiveNodeLayer().removeAttribute("active");
+				switchActiveLayerTo(e.target);
 
 				var shadow_ID = e.target.getAttribute("sid") | 0;
 				layer_ID = order.indexOf(shadow_ID);
 
-				console.log("layer_ID", layer_ID);
-
 				Tool.setActiveShadow(shadow_ID);
-				e.target.setAttribute("active", "true");
+			}
+
+			if (e.target.className === "delete") {
+				var layer = e.target.parentNode;
+				var shadow_ID = layer.getAttribute("sid") | 0;
+				var index = order.indexOf(shadow_ID);
+
+				console.log(shadow_ID, index);
+
+				order.splice(index, 1);
+				stack.removeChild(layer);
+				Tool.deleteShadow(shadow_ID);
+				if (activeLayer === layer)
+					activateSubject();
 			}
 		}
 
-		var getActiveLayerID = function getActiveLayerID() {
-		}
-
-		var activeLayer = function activeLayer(elem) {
-
-		}
-
 		var getActiveNodeLayer = function getActiveNodeLayer() {
-			if(layer_ID == null)
+			if(stack.childElementCount === 0)
 				return null;
 
 			return stack.children[layer_ID];
 		}
 
+		var switchActiveLayerTo = function switchActiveLayerTo(node) {
+			console.log("activeLayer", activeLayer);
+
+			if (activeLayer !== null)
+				activeLayer.removeAttribute("active");
+
+
+			activeLayer = node;
+			activeLayer.setAttribute("active", "true");
+		}
+
 		var addLayer = function addLayer() {
 			var layer = createLayer();
-			var activeNode = getActiveNodeLayer();
-
-			// DEBUG
-			console.log("before_layer_ID", layer_ID);
-			console.log("activeNode", activeNode);
-
-			if (activeNode !== null)
-				activeNode.removeAttribute("active");
-
-			if (layer_ID === null)
-				layer_ID = 0;
-
+			switchActiveLayerTo(layer);
 			Tool.addShadow();
-
-			stack.insertBefore(layer, activeNode);
+			stack.insertBefore(layer, getActiveNodeLayer());
 			order.splice(layer_ID, 0, nr++);
-
-			// DEBUG
-			console.log("after_layer_ID", layer_ID);
 			console.log(order);
 		}
 
 		var createLayer = function createLayer() {
 			var elem = document.createElement("div");
+			var del = document.createElement("span");
 			elem.className = 'layer';
 			elem.setAttribute('sid', nr);
-			elem.setAttribute('active', "true");
 			elem.textContent = 'shadow ' + nr;
+			del.className = 'delete';
+			elem.appendChild(del);
 			return elem;
 		}
 
