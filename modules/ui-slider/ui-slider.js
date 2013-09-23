f2282a9de20c28a8d6442c6d3fe1029449f63049
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2013-09-24 00:07:11 +0300

    changed module instantiation to a simpler format

--- modules/ui-slider/ui-slider.js
+++ modules/ui-slider/ui-slider.js
@@ -1,21 +1,43 @@
-'use strict';
-
 /**
  * UI-SlidersManager
  */
 
 var SliderManager = (function SliderManager() {
+	'use strict';
 
 	var subscribers = {};
 	var sliders = [];
 
+	function trackMouse(elem, callback, startFunc, endFunc) {
+		startFunc = startFunc || function(e) {};
+		endFunc = endFunc || function(e) {};
+
+		elem.addEventListener('mousedown', function(e) {
+			e.preventDefault();
+			startFunc(e);
+
+			document.addEventListener('mousemove', callback);
+			document.addEventListener('mouseup', function up(e) {
+				document.removeEventListener('mousemove', callback);
+				document.removeEventListener('mouseup', up);
+				endFunc(e);
+			});
+		});
+
+		elem.addEventListener('click', function(e) {
+			e.stopPropagation();
+		});
+	}
+
 	var Slider = function(node) {
+		var topic = node.getAttribute('data-topic');
+		var info = node.getAttribute('data-info');
+		var unit = node.getAttribute('data-unit');
 		var min = node.getAttribute('data-min') | 0;
 		var max = node.getAttribute('data-max') | 0;
 		var step = node.getAttribute('data-step') | 0;
 		var value = node.getAttribute('data-value') | 0;
 		var snap = node.getAttribute('data-snap');
-		var topic = node.getAttribute('data-topic');
 
 		this.min = min;
 		this.max = max > 0 ? max : 100;
@@ -23,92 +45,74 @@ var SliderManager = (function SliderManager() {
 		this.value = value <= max && value >= min ? value : (min + max) / 2 | 0;
 		this.snap = snap === "true" ? true : false;
 		this.topic = topic;
+		this.unit = unit;
+
+		var name = document.createElement('div');
+		var slider = document.createElement('div');
+		var pointer = document.createElement('div');
+
+		name.className = 'ui-slider-name';
+		name.textContent = info;
+
+		slider.className = 'ui-slider-slider';
+		pointer.className = 'ui-slider-pointer';
+
+		node.appendChild(name);
+		slider.appendChild(pointer);
+
+		this.pointer = pointer;
+		this.slider = slider;
 		this.node = node;
 
-		var pointer = document.createElement('div');
-		pointer.className = 'ui-slider-pointer';
-		node.appendChild(pointer);
-		this.pointer = pointer;
+		this.createSetButton('-', this.decrement.bind(this));
+		node.appendChild(slider);
+		this.createSetButton('+', this.increment.bind(this));
+		this.createInputField();
 
-		setMouseTracking(node, updateSlider.bind(this));
+		new trackMouse(slider, this.updateSlider.bind(this), this.startMove.bind(this));
+		slider.addEventListener('click', function(e) {
+			this.startMove();
+			this.updateSlider(e);
+		}.bind(this));
 
 		sliders[topic] = this;
-		setValue(topic, this.value);
-	}
+		this.setValue(this.value);
+	};
 
-	var setButtonComponent = function setButtonComponent(node) {
-		var type = node.getAttribute('data-type');
-		var topic = node.getAttribute('data-topic');
-		if (type === "sub") {
-			node.textContent = '-';
-			node.addEventListener("click", function() {
-				decrement(topic);
-			});
-		}
-		if (type === "add") {
-			node.textContent = '+';
-			node.addEventListener("click", function() {
-				increment(topic);
-			});
-		}
-	}
+	Slider.prototype.createSetButton = function createSetButton(type, callback) {
+		var button = document.createElement('div');
+		button.className = 'ui-slider-button';
+		button.textContent = type;
+		button.addEventListener("click", callback);
+		this.node.appendChild(button);
+	};
 
-	var setInputComponent = function setInputComponent(node) {
-		var topic		= node.getAttribute('data-topic');
-		var unit_type	= node.getAttribute('data-unit');
-
+	Slider.prototype.createInputField = function createInputField() {
 		var input = document.createElement('input');
-		var unit = document.createElement('span');
-		unit.textContent = unit_type;
-
 		input.setAttribute('type', 'text');
-		node.appendChild(input);
-		node.appendChild(unit);
+		this.node.appendChild(input);
 
 		input.addEventListener('click', function(e) {
 			this.select();
 		});
 
 		input.addEventListener('change', function(e) {
-			setValue(topic, e.target.value | 0);
-		});
+			this.setValue(e.target.value | 0);
+		}.bind(this));
 
-		subscribe(topic, function(value) {
-			node.children[0].value = value;
-		});
-	}
-
-	var increment = function increment(topic) {
-		var slider = sliders[topic];
-		if (slider === null || slider === undefined)
-			return;
-
-		if (slider.value + slider.step <= slider.max) {
-			slider.value += slider.step;
-			setValue(slider.topic, slider.value)
-			notify.call(slider);
-		}
+		subscribe(this.topic, function(value) {
+			input.value = value + this.unit;
+		}.bind(this));
 	};
 
-	var decrement = function decrement(topic) {
-		var slider = sliders[topic];
-		if (slider === null || slider === undefined)
-			return;
+	Slider.prototype.startMove = function startMove(e) {
+		this.sliderX = this.slider.getBoundingClientRect().left;
+	};
 
-		if (slider.value - slider.step >= slider.min) {
-			slider.value -= slider.step;
-			setValue(topic, slider.value)
-			notify.call(slider);
-		}
-	}
-
-	// this = Slider object
-	var updateSlider = function updateSlider(e) {
-		var node = this.node;
-		var pos = e.pageX - node.offsetLeft;
-		var width = node.clientWidth;
+	Slider.prototype.updateSlider = function updateSlider(e) {
+		var width = this.slider.clientWidth;
+		var pos = e.clientX - this.sliderX;
 		var delta = this.max - this.min;
-		var offset = this.pointer.clientWidth + 4; // border width * 2
 
 		if (pos < 0) pos = 0;
 		if (pos > width) pos = width;
@@ -116,88 +120,74 @@ var SliderManager = (function SliderManager() {
 		var value = pos * delta / width | 0;
 		var precision = value % this.step;
 		value = value - precision + this.min;
+
 		if (precision > this.step / 2)
 			value = value + this.step;
 
 		if (this.snap)
 			pos =  (value - this.min) * width / delta;
 
-		this.pointer.style.left = pos - offset/2 + "px";
+		this.pointer.style.left = pos + "px";
 		this.value = value;
-		node.setAttribute('data-value', value);
 		notify.call(this);
-	}
+	};
+
+	Slider.prototype.increment = function increment() {
+		this.setValue(this.value + this.step);
+	};
+
+	Slider.prototype.decrement = function decrement() {
+		this.setValue(this.value - this.step);
+	};
+
+	Slider.prototype.setValue = function setValue(value) {
+		if (value > this.max || value < this.min)
+			return;
+
+		var delta = this.max - this.min;
+		var width = this.slider.clientWidth;
+		var pos =  (value - this.min) * width / delta | 0;
+		this.value = value;
+		this.pointer.style.left = pos + "px";
+		notify.call(this);
+	};
 
 	var setValue = function setValue(topic, value) {
 		var slider = sliders[topic];
-
-		if (value > slider.max || value < slider.min)
-			return;
-
-		var delta = slider.max - slider.min;
-		var width = slider.node.clientWidth;
-		var offset = slider.pointer.clientWidth;
-		var pos =  (value - slider.min) * width / delta;
-		slider.value = value;
-		slider.pointer.style.left = pos - offset / 2 + "px";
-		slider.node.setAttribute('data-value', value);
-		notify.call(slider);
-	}
-
-	var setMouseTracking = function setMouseTracking(elem, callback) {
-		elem.addEventListener("mousedown", function(e) {
-			callback(e);
-			document.addEventListener("mousemove", callback);
-		});
-
-		document.addEventListener("mouseup", function(e) {
-			document.removeEventListener("mousemove", callback);
-		});
-	}
+		slider.setValue(value);
+	};
 
 	var subscribe = function subscribe(topic, callback) {
 		if (subscribers[topic] === undefined)
 			subscribers[topic] = [];
 		subscribers[topic].push(callback);
-	}
+	};
 
 	var unsubscribe = function unsubscribe(topic, callback) {
 		subscribers[topic].indexOf(callback);
 		subscribers[topic].splice(index, 1);
-	}
+	};
 
 	var notify = function notify() {
 		if (subscribers[this.topic] === undefined)
 			return;
 		for (var i = 0; i < subscribers[this.topic].length; i++)
 			subscribers[this.topic][i](this.value);
-	}
+	};
 
 	var init = function init() {
-		var elem, size;
-
-		elem = document.querySelectorAll('.ui-slider-btn-set');
-		size = elem.length;
-		for (var i = 0; i < size; i++)
-			setButtonComponent(elem[i]);
-
-		elem = document.querySelectorAll('.ui-slider-input');
-		size = elem.length;
-		for (var i = 0; i < size; i++)
-			setInputComponent(elem[i]);
-
-		elem = document.querySelectorAll('.ui-slider');
-		size = elem.length;
+		var elem = document.querySelectorAll('.ui-slider');
+		var size = elem.length;
 		for (var i = 0; i < size; i++)
 			new Slider(elem[i]);
-	}
+	};
 
 	return {
 		init : init,
 		setValue : setValue,
 		subscribe : subscribe,
 		unsubscribe : unsubscribe
-	}
+	};
 
 })();
 
